d915e4c25cc1d110739143537478073eb0e597b3
Luk <luk@ikeloud>
2025-11-22 19:12:21 +0100

    feat: benchmarking using a python script that outputs a csv file.

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,4 +1,5 @@
 from io import TextIOWrapper
+import re
 import subprocess
 import shutil
 import sys
@@ -6,14 +7,19 @@ import os
 import itertools
 
 
-TIME_REGEX_PART = r"(\d+\.\d+) seconds."
-BYTES_REGEX_PART = r"(\d+) Bytes."
+NUMBER_REGEX_PART = r"(\d+(?:.\d+)?(?:e[+-]?\d+)?)"
+TIME_REGEX_PART = rf"{NUMBER_REGEX_PART} seconds\."
+BYTES_REGEX_PART = rf"{NUMBER_REGEX_PART} Bytes\."
 TIME_TOTAL_REGEX = rf"Time Kernel Total : {TIME_REGEX_PART}"
 TIME_KERNEL_REGEX = rf"Time Spent Simulating : {TIME_REGEX_PART}"
 TIME_SNAPSHOTS_REGEX = rf"Time Making and Saving Snapshots : {TIME_REGEX_PART}"
 TIME_SISMOS_REGEX = rf"Time Making Sismos : {TIME_REGEX_PART}"
 TIME_OUTPUT_SISMOS_REGEX = rf"Time Saving Outputs : {TIME_REGEX_PART}"
 TOTAL_BYTES_REGEX = rf"Total written data: {BYTES_REGEX_PART}"
+BYTES_RECEIVERS_REGEX = rf"receiver_\d+: {BYTES_REGEX_PART}"
+SNAPSHOT_RECEIVERS_REGEX = rf"snapshot\d+.bin: {BYTES_REGEX_PART}"
+
+print(TIME_TOTAL_REGEX)
 
 
 def benchmark(
@@ -28,7 +34,7 @@ def benchmark(
     receivers: list[list[tuple[int, int, int]]],
 ):
     output_file.write(
-        "ex|ey|ez|order|format|receivers|global|kernel|make_snapshots|make_sismos|output_sismos|total_snap_bytes|total_sismos_bytes\n"
+        "ex|ey|ez|order|format|receivers|global|kernel|make_snapshots|make_sismos|output_sismos|total_bytes|total_snap_bytes|total_sismos_bytes\n"
     )
     tmp_path = os.path.join(output_dir, "tmp")
     if os.path.exists(tmp_path):
@@ -66,10 +72,36 @@ def benchmark(
         os.remove(output_receivers)
         shutil.rmtree(snaps)
 
-        # TODO: extract measures
+        # TODO: make this safer
         with open(measures, "r") as measures_file:
-            print(measures_file.read())
-        # TODO: put in CSV
+            measures_file_content = measures_file.read()
+            try:
+                time_total = re.findall(TIME_TOTAL_REGEX, measures_file_content)[0]
+                time_kernel = re.findall(TIME_KERNEL_REGEX, measures_file_content)[0]
+                time_snaps = re.findall(TIME_SNAPSHOTS_REGEX, measures_file_content)[0]
+                time_making_sismos = re.findall(
+                    TIME_SISMOS_REGEX, measures_file_content
+                )[0]
+                time_saving_sismos = re.findall(
+                    TIME_OUTPUT_SISMOS_REGEX, measures_file_content
+                )[0]
+                total_written_data = re.findall(
+                    TOTAL_BYTES_REGEX, measures_file_content
+                )[0]
+                written_receiver_data = re.findall(
+                    BYTES_RECEIVERS_REGEX, measures_file_content
+                )[0]
+                snapshots_receiver_data = re.findall(
+                    SNAPSHOT_RECEIVERS_REGEX, measures_file_content
+                )
+                total_snap_bytes = sum(map(lambda x: int(x), snapshots_receiver_data))
+                csv_line = f"{ex}|{ey}|{ez}|{order}|{format}|{len(receivers[0])}|{time_total}|{time_kernel}|{time_snaps}|{time_making_sismos}|{time_saving_sismos}|{total_written_data}|{total_snap_bytes}|{written_receiver_data}\n"
+                output_file.write(csv_line)
+            except Exception as e:
+                print(f"Exception occured: {e}", file=sys.stderr)
+                print(measures_file_content)
+                shutil.rmtree(tmp_path)
+                break
         os.remove(measures)
         done += 1
 
@@ -77,12 +109,14 @@ def benchmark(
 if __name__ == "__main__":
     OUTPUT_DIR = "./exp1"
     OUTPUT_FILE = "result.csv"
-    EXS = [5]
-    EYS = [5]
-    EZS = [5]
+    EXS = [10]
+    EYS = [10]
+    EZS = [10]
     ORDERS = [1]
     FORMATS = ["bin", "plain"]
-    RECEIVERS = [[(1, 4, 3), (122, 312, 451)]]
+    RECEIVERS = [
+        [(1, 4, 3), (122, 312, 451)],
+    ]
     BIN = "./build/debug/bin/semproxy"
 
     COMPILE = True
