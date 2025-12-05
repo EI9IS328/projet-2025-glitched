136a1cfbef7ab88c5aa316ac9ad92fbc591fe5da
LECTAKONE Eric <Eric.Lectakone@bordeaux-inp.fr>
2025-12-05 15:50:05 +0100

    fix: Python visualizer with header

--- src/visualizer/nodeCalc.py
+++ src/visualizer/nodeCalc.py
@@ -1,35 +1,42 @@
 import os
+def getMetaDim():
+    file = "snapshot600.bin"
+    dir = os.path.dirname(__file__)
+    path = os.path.normpath(os.path.join(dir, '..', '..', "snapshot", file))
+    with open (file=path, newline='') as f:
+        line = f.readline().strip("\n")
+        data = line.split(",")
 
-ex = 10
-ey = 10
-ez = 10
-order = 2
+    return int(data[0]), int(data[1]), int(data[2]), int(data[3])
 
+ex, ey, ez, order = getMetaDim()
 
-nx = order * ex + 1
-ny = order * ey + 1
-nz = order * ez + 1
-
-def globalNodeIndex(el, i, j, k):
-    elemZ = el // (ex * ey)
-    tmp = el % (ex * ey)
-    elemY = tmp // ex
-    elemX = tmp % ex
-
-    ix = elemX * order + i
-    iy = elemY * order + j
-    iz = elemZ * order + k
-
-    return ix + iy * nx + iz * nx * ny
 
 
 def getSnapshotData(iter):
+    nx = order * ex + 1
+    ny = order * ey + 1
+    nz = order * ez + 1
+
+    def globalNodeIndex(el, i, j, k):
+        elemZ = el // (ex * ey)
+        tmp = el % (ex * ey)
+        elemY = tmp // ex
+        elemX = tmp % ex
+
+        ix = elemX * order + i
+        iy = elemY * order + j
+        iz = elemZ * order + k
+
+        return ix + iy * nx + iz * nx * ny
+    
     iterData = {}
     iter = str(iter)
     file = "snapshot" + iter + ".bin"
     dir = os.path.dirname(__file__)
     path = os.path.normpath(os.path.join(dir, '..', '..', "snapshot", file))
     with open(file=path, newline='') as snapshot:
+        next(snapshot)
         el = 0
         for row in snapshot:
             nodeIdx = 0
@@ -44,6 +51,8 @@ def getSnapshotData(iter):
             el+=1
     return iterData
 
+
+
 def nodeCoord(globalIdx):
     nodesPerDim = [ex * order + 1 , ey * order + 1, ez * order + 1]
     
@@ -55,4 +64,3 @@ def nodeCoord(globalIdx):
     nodeIdx = [i, j, k]
 
     return nodeIdx
-
