1816a0e8d066c7c56d1fd03edd377c8394fb6321
LECTAKONE Eric <Eric.Lectakone@bordeaux-inp.fr>
2025-12-18 16:26:51 +0100

    feat: node pos and pres from binary

--- src/visualizer/binDecode.py
+++ src/visualizer/binDecode.py
@@ -16,12 +16,46 @@ def openBin():
     dir = os.path.dirname(__file__)
     path = os.path.normpath(os.path.join(dir, '..', '..', "snapshot", file))
 
+    iterData = {}
+
     with open(path, "rb") as f:
         ex, ey, ez, order = struct.unpack(HEADER_FORMAT, f.read(HEADER_SIZE))
         
+        nx = order * ex + 1
+        ny = order * ey + 1
+        nz = order * ez + 1
+
+        def globalNodeIndex(el, i, j, k):
+            elemZ = el // (ex * ey)
+            tmp = el % (ex * ey)
+            elemY = tmp // ex
+            elemX = tmp % ex
+
+            ix = elemX * order + i
+            iy = elemY * order + j
+            iz = elemZ * order + k
+
+            return ix + iy * nx + iz * nx * ny
+
         row_bytes = f.read(ROW_SIZE)
-        print(struct.unpack(ROW_FORMAT, row_bytes))
+        el = 0
+        while len(row_bytes) != 0:
+            nodeIdx = 0
+            nodes = struct.unpack(ROW_FORMAT, row_bytes)
+
+            for nodePrs in nodes:
+                nodeIdx +=1
+                x = nodeIdx % (order + 1)
+                y = (nodeIdx // (order + 1) % (order + 1))
+                z = nodeIdx // (order + 1) * (order + 1)
+                iterData[globalNodeIndex(el,x,y,z)] = nodePrs
+
+            el += 1
+            row_bytes = f.read(ROW_SIZE)
+        
+        return iterData
+
 
 
 if __name__ == "__main__":
-    openBin()
+    print(openBin())
