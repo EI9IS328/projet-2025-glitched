c5e99e383bb6fb3c973ea3cfec86e6baf91dcee3
LECTAKONE Eric <Eric.Lectakone@bordeaux-inp.fr>
2026-01-17 15:25:20 +0100

    feat: Unified code for discerning .txt and .bin

--- src/visualizer/nodeCalc.py
+++ src/visualizer/nodeCalc.py
@@ -9,79 +9,29 @@ def getMetaDim(iteration):
         with open(base_path + ".bin", "rb") as f:
             data = f.read(16)
             ex, ey, ez, order = struct.unpack("<iiii", data)
-            return ex, ey, ez, order, True # True means it IS binary
+            return {"ex": ex, "ey": ey, "ez": ez, "order": order, "is_bin": True}
             
     elif os.path.exists(base_path + ".txt"):
         with open(base_path + ".txt", "r") as f:
             line = f.readline().strip().split(",")
-            return int(line[0]), int(line[1]), int(line[2]), int(line[3]), False # False means text
+            return {"ex": int(line[0]), "ey": int(line[1]), "ez": int(line[2]), "order": int(line[3]), "is_bin": False}
             
     return None
 
-ex, ey, ez, order, is_bin = getMetaDim(0)
+def get_node_coords(globalIdx, meta):
+    nx = meta['ex'] * meta['order'] + 1
+    ny = meta['ey'] * meta['order'] + 1
+    nz = meta['ez'] * meta['order'] + 1
 
-
-def getSnapshotData(iter):
-    nx = order * ex + 1
-    ny = order * ey + 1
-    nz = order * ez + 1
-
-    def globalNodeIndex(el, i, j, k):
-        elemZ = el // (ex * ey)
-        tmp = el % (ex * ey)
-        elemY = tmp // ex
-        elemX = tmp % ex
-
-        ix = elemX * order + i
-        iy = elemY * order + j
-        iz = elemZ * order + k
-
-        return ix + iy * nx + iz * nx * ny
-    
-    iterData = {}
-    iter = str(iter)
-    file = "snapshot" + iter + ".bin"
-    dir = os.path.dirname(__file__)
-    path = os.path.normpath(os.path.join(dir, '..', '..', "snapshot", file))
-    with open(file=path, newline='') as snapshot:
-        next(snapshot)
-        el = 0
-        for row in snapshot:
-            nodeIdx = 0
-            nodes = row.split(",")
-            for nodePrs in nodes:
-                nodePrs = nodePrs.replace("\n", "")
-                nodeIdx += 1
-                x = nodeIdx % (order + 1)
-                y = (nodeIdx // (order + 1)) % (order + 1)
-                z = nodeIdx // ( (order + 1) * (order + 1)) 
-                iterData[globalNodeIndex(el,x,y,z)] = nodePrs
-            el+=1
-    return iterData
-
-
-
-def nodeCoord(globalIdx):
-    nodesPerDim = [ex * order + 1 , ey * order + 1, ez * order + 1]
-    
-    k = globalIdx // (nodesPerDim[0] * nodesPerDim[1])
-    remainder = globalIdx % (nodesPerDim[0] * nodesPerDim[1])
-    j = remainder // nodesPerDim[0]
-    i = remainder % nodesPerDim[0]
-
-    nodeIdx = [i, j, k]
-
-    return nodeIdx
-
-def nodeCoordBin(globalIdx, ex, ey, ez, order):
-    nx = ex * order + 1
-    ny = ey * order + 1
-    nz = ez * order + 1
-
-
-    k = globalIdx // (nx * ny)
-    remainder = globalIdx % (nx * ny)
-    j = remainder // nx
-    i = remainder % nx
+    if meta['is_bin']:
+        i = globalIdx // (ny * nz)
+        remainder = globalIdx % (ny * nz)
+        j = remainder // nz
+        k = remainder % nz
+    else:
+        k = globalIdx // (nx * ny)
+        remainder = globalIdx % (nx * ny)
+        j = remainder // nx
+        i = remainder % nx
 
     return [i, j, k]
--- src/visualizer/visualizer.py
+++ src/visualizer/visualizer.py
@@ -1,103 +1,46 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-import numpy as np
 import nodeCalc
 import binDecode
 
-def getSlice(nodes, z):
-    prsMatrix = [(nodeCalc.ey * nodeCalc.order + 1) * [0] \
-                  for i in range(nodeCalc.ex * nodeCalc.order + 1)]
-    for key, value in nodes.items():
-        nodeCoord = nodeCalc.nodeCoord(key)
-        if nodeCoord[2] == z:
-            prsMatrix[nodeCoord[0]][nodeCoord[1]] = float(value)
-    return prsMatrix
-
-def getSliceBin(nodes, z, iteration):
+def get_slice_data(nodes, z_target, iteration):
     meta = nodeCalc.getMetaDim(iteration)
     if not meta:
-        return []
+        raise FileNotFoundError(f"Snapshot {iteration} not found.")
     
-    ex, ey, ez, order, is_bin = meta
+    nx = meta['ex'] * meta['order'] + 1
+    ny = meta['ey'] * meta['order'] + 1
     
-    nx, ny = (ex * order + 1), (ey * order + 1)
     prsMatrix = [[0.0 for _ in range(ny)] for _ in range(nx)]
     
     for key, value in nodes.items():
-        if is_bin:
-            coord = nodeCalc.nodeCoordBin(key, ex, ey, ez, order)
-        else:
-            coord = nodeCalc.nodeCoord(key) 
-            
-        if coord[2] == z:
-            prsMatrix[coord[0]][coord[1]] = float(value)
+        coord = nodeCalc.get_node_coords(key, meta)
+        
+        if coord[2] == z_target:
+            if 0 <= coord[0] < nx and 0 <= coord[1] < ny:
+                prsMatrix[coord[0]][coord[1]] = float(value)
             
     return prsMatrix
 
-
-def heatmap(slice):
-
-    min_val = min(min(row) for row in slice)
-    max_val = max(max(row) for row in slice)
-
-    fig, ax = plt.subplots(figsize=(12, 3))
-    im = ax.imshow(slice, 
-           norm=colors.SymLogNorm(linthresh=1e-10, linscale=1, vmin=min_val, vmax=max_val),
-           aspect='equal') # This ensures squares stay square
-
-    plt.colorbar(im, ax=ax, label='Pressure (Pa)', pad=0.02, aspect=10)
-
-    plt.title("Customized heatmap with annotations")
-    plt.xlabel("X-axis")
-    plt.ylabel("Y-axis")
-    plt.show()
-
-def surface3d(nodes):
-    Zlevels = [i for i in range(nodeCalc.ez * nodeCalc.order + 1)]
-
-    nx = np.arange(nodeCalc.order * nodeCalc.ex + 1)
-    ny = np.arange(nodeCalc.order * nodeCalc.ey + 1)
-    Xg, Yg = np.meshgrid(nx, ny)
-
-    min_val = min(np.min(np.array(getSlice(nodes, Z))) for Z in Zlevels)
-    max_val = max(np.max(np.array(getSlice(nodes, Z))) for Z in Zlevels)
-
-    norm = colors.SymLogNorm(linthresh=1e-10, linscale=1, vmin=min_val, vmax=max_val)
-    cmap = plt.cm.viridis
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    for z in Zlevels:
-        Pi = getSlice(nodes, z)
-        Pi = np.array(Pi)
-        Zg = np.full_like(Pi, z)
-        facecolors = cmap(norm(Pi))
-
-        ax.plot_surface(
-            Xg, Yg, Zg,
-            facecolors=facecolors,
-            rstride=1,
-            cstride=1,
-            shade=False  
-        )
-
-    mappable = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
-    mappable.set_array([])  # required
-    plt.colorbar(mappable, ax=ax, shrink=0.5, pad=0.1)
-
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-
+def heatmap(data_slice):
+    plt.figure(figsize=(10, 8))
+    im = plt.imshow(data_slice, origin='lower', cmap='viridis', aspect='auto')
+    plt.colorbar(im, label='Pressure (Pa)')
+    plt.xlabel("X-axis (Nodes)")
+    plt.ylabel("Y-axis (Nodes)")
+    plt.title("Pressure Slice")
     plt.show()
 
 if __name__ == "__main__":
-    # values = nodeCalc.getSnapshotData(500)
-    values = binDecode.openBin(600)
-    # print(values)
-    # surface3d(values)
-    # print(getSlice(values, 5))
-    matrix = getSliceBin(values, 25, 600)
-    heatmap(matrix)
+    ITERATION = 1200
+    Z_PLANE = 25
 
+    meta = nodeCalc.getMetaDim(ITERATION)
+    if meta and meta['is_bin']:
+        data = binDecode.openBin(ITERATION)
+    else:
+        data = nodeCalc.getSnapshotData(ITERATION) 
+
+    if data:
+        matrix = get_slice_data(data, Z_PLANE, ITERATION)
+        heatmap(matrix)
