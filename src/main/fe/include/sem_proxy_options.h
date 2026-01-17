7beeab22cf40fdaa769a17ff6d7e6bc818d83682
Lucas M. <lucas.marques@u-bordeaux.fr>
2026-01-17 19:49:41 +0100

    fix: validate quant level

--- src/main/fe/include/sem_proxy_options.h
+++ src/main/fe/include/sem_proxy_options.h
@@ -53,13 +53,13 @@ class SemProxyOptions
 
   std::string _str_tolower(std::string s)
   {
-      std::transform(s.begin(), s.end(), s.begin(),
-                  // static_cast<int(*)(int)>(std::tolower)         // wrong
-                  // [](int c){ return std::tolower(c); }           // wrong
-                  // [](char c){ return std::tolower(c); }          // wrong
-                     [](unsigned char c){ return std::tolower(c); } // correct
-                    );
-      return s;
+    std::transform(s.begin(), s.end(), s.begin(),
+                   // static_cast<int(*)(int)>(std::tolower)         // wrong
+                   // [](int c){ return std::tolower(c); }           // wrong
+                   // [](char c){ return std::tolower(c); }          // wrong
+                   [](unsigned char c) { return std::tolower(c); }  // correct
+    );
+    return s;
   }
 
   void _testFile(std::string filePath)
@@ -138,12 +138,20 @@ class SemProxyOptions
         snapshot_slice_axis != "Y" && snapshot_slice_axis != "y" &&
         snapshot_slice_axis != "Z" && snapshot_slice_axis != "z")
     {
-      throw std::runtime_error(
-          "snapshot-slice-axis must be X, Y, or Z. Got: " + snapshot_slice_axis);
+      throw std::runtime_error("snapshot-slice-axis must be X, Y, or Z. Got: " +
+                               snapshot_slice_axis);
     }
     compression_method_ = _str_tolower(compression_method_);
-    if (compression_method_ != "none" && compression_method_ != "rle" && compression_method_ != "quant") {
-      throw std::runtime_error("unknown compression method " + compression_method_);
+    if (compression_method_ != "none" && compression_method_ != "rle" &&
+        compression_method_ != "quant")
+    {
+      throw std::runtime_error("unknown compression method " +
+                               compression_method_);
+    }
+    if (quant_level_ != 1 && quant_level_ != 2 && quant_level_ != 4)
+    {
+      throw std::runtime_error("unsupported quantization level: " +
+                               std::to_string(quant_level_));
     }
   }
 
@@ -199,9 +207,11 @@ class SemProxyOptions
         "snapshot-format", "snapshot format, bin|plain",
         cxxopts::value<std::string>(o.snapshot_format))(
         "compression", "Use compression for binary snapshots (None or RLE)",
-        cxxopts::value<std::string>(o.compression_method_))("quant-level", "number of bytes to quantize, 1|2|4",
+        cxxopts::value<std::string>(o.compression_method_))(
+        "quant-level", "number of bytes to quantize, 1|2|4",
         cxxopts::value<int>(o.quant_level_))(
-        "snapshot-colormap", "snapshot colormap for in-situ, grayscale|viridis|jet",
+        "snapshot-colormap",
+        "snapshot colormap for in-situ, grayscale|viridis|jet",
         cxxopts::value<std::string>(o.snapshot_colormap))(
         "watched-receivers",
         "Path for a list of watchedReceivers to save values from",
